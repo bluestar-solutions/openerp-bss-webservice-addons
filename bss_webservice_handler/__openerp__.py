8863d24f78978a64d8d838a12e2687a44371d0c6
Hervé Martinet <herve.martinet@blues2.ch>
2013-08-29 11:34:40 +0200

    Update version

--- bss_webservice_handler/__openerp__.py
+++ bss_webservice_handler/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     "name": "Webservice Handler",
-    'version': '7.0.2.0-1',
+    'version': '7.0.2.0-2',
     "category" : "Bluestar/Addons/WS",
     "complexity": "easy",
     "description": """
